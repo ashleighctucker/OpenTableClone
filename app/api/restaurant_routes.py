e5d17c97ca6708b97fffd9972c19e8e553b64e8d
ashleighctucker <ashleighctucker@gmail.com>
2021-12-06 15:58:41 -0800

    added get route for restaurants, tested

--- app/api/restaurant_routes.py
+++ app/api/restaurant_routes.py
@@ -1,11 +1,12 @@
 from flask import Blueprint, jsonify
 from flask_login import login_required
-from app.models import Restaurant, restaurant
+from app.models import Restaurant
 
 restaurant_routes = Blueprint('restaurants', __name__)
 
 
 @restaurant_routes.route('/')
 def get_restaurants():
-    restaurants = Restaurant.query.all() # to-do eager load reservations and reviews
-    
+    # to-do eager load reservations and reviews
+    restaurants = Restaurant.query.all()
+    return {'restaurants': [restaurant.to_dict() for restaurant in restaurants]}
