7722947c595793be1f208e85778058e0b2c9dc11
ashleighctucker <ashleighctucker@gmail.com>
2021-12-06 16:32:35 -0800

    changed to_dict for restaurants to render cusine type as a string

--- app/models/cuisine.py
+++ app/models/cuisine.py
@@ -7,7 +7,12 @@ class Cuisine(db.Model):
     __tablename__ = 'cuisines'
     id = db.Column(db.Integer, primary_key=True)
     type = db.Column(db.String(100), nullable=False, unique=True)
-    restaurant_cuisines = db.relationship('Restaurant', back_populates='cuisine')
+    restaurant_cuisines = db.relationship(
+        'Restaurant', back_populates='cuisine')
+
     def to_dict(self):
         return {
             'type': self.type}
+
+    def __repr__(self):
+        return self.type
