b9973f7e2bb37453c3818c5a1309d69aa8ae20b8
jas0123uah <jas0123@uah.edu>
2021-12-08 13:49:30 -0500

    Import Restaurant Owner reservation form & Customer reservation form.

--- app/forms/__init__.py
+++ app/forms/__init__.py
@@ -2,6 +2,6 @@ from .login_form import LoginForm
 from .signup_form import SignUpForm
 from .post_restaurant_form import NewRestaurant
 from .favorite_form import FavoriteForm
-from .reservation_form import ReservationForm
+from .restaurant_owner_reservation_form import RestaurantOwnerReservationForm
+from .customer_reservation_form import CustomerReservationForm
 from .post_review_form import NewReview
-
