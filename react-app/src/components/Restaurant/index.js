5da712c9abdf19144af4fce9ac62e76e0b65ebc7
Izzy <snakedreamz134@gmail.com>
2021-12-09 17:35:23 -0500

    sigh

--- react-app/src/components/Restaurant/index.js
+++ react-app/src/components/Restaurant/index.js
@@ -1,5 +1,4 @@
-
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { deleteReview, getRestaurants } from '../../store/restaurant';
 
@@ -9,26 +8,34 @@ import EditReviewModal from '../EditReview/EditReviewModal';
 import CustomerBookReservationModal from '../CustomerBookReservation';
 const Restaurant = () => {
   const { restaurantId } = useParams();
-  const [date, setDate]= useState("");
+  const [date, setDate] = useState('');
   const restaurant = useSelector((state) => state.restaurants[+restaurantId]);
   const dispatch = useDispatch();
 
   const { reviews: rawReviews } = useSelector(
     (state) => state.restaurants[restaurantId]
+  );
 
-  let allReservations = useSelector((state) =>state.restaurants?.[restaurantId]?.reservations)
-  let availableReservationsArray = allReservations.filter(res => res.booked === false).sort(function(a,b){
-//                 // Turn your strings into dates, and then subtract them
-//                 // to get a value that is either negative, positive, or zero.
-                return new Date(a.date) - new Date(b.date);
-             })
-  console.log(availableReservationsArray)
-  let reservationsByDate = availableReservationsArray.filter((reservation) => reservation.date == date) 
-  let arrayOfAvailableDates= availableReservationsArray.map((reservation) => reservation.date)
+  let allReservations = useSelector(
+    (state) => state.restaurants?.[restaurantId]?.reservations
+  );
+  let availableReservationsArray = allReservations
+    .filter((res) => res.booked === false)
+    .sort(function (a, b) {
+      //                 // Turn your strings into dates, and then subtract them
+      //                 // to get a value that is either negative, positive, or zero.
+      return new Date(a.date) - new Date(b.date);
+    });
+  console.log(availableReservationsArray);
+  let reservationsByDate = availableReservationsArray.filter(
+    (reservation) => reservation.date == date
+  );
+  let arrayOfAvailableDates = availableReservationsArray.map(
+    (reservation) => reservation.date
+  );
   let reviews;
   const rawReviews = useSelector(
-    (state) =>
-      state.restaurants[restaurantId]?.reviews
+    (state) => state.restaurants[restaurantId]?.reviews
   );
 
   const deleteOneReview = (id) => {
@@ -39,7 +46,10 @@ const Restaurant = () => {
   return (
     <div className="restaurant-container">
       <h1>Single Restaurant Page</h1>
-      <CustomerBookReservationModal arrayOfAvailableDates={arrayOfAvailableDates} availableReservationsArray={availableReservationsArray} />
+      <CustomerBookReservationModal
+        arrayOfAvailableDates={arrayOfAvailableDates}
+        availableReservationsArray={availableReservationsArray}
+      />
       <h1>{restaurant?.name}</h1>
       <div>
         {Object.values(rawReviews)?.map((review) => {
