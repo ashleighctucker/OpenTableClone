c8a07f3d83b24abcef2f496b3ebb19903b2f1f8f
Cam Tangalakis-Lippert <cami.lippert@gmail.com>
2021-12-09 08:31:51 -0800

    single restaurant page

--- react-app/src/components/Restaurant/index.js
+++ react-app/src/components/Restaurant/index.js
@@ -3,10 +3,13 @@ import { useSelector, useDispatch } from 'react-redux';
 import { deleteReview } from '../../store/restaurant';
 import { useParams } from 'react-router';
 import EditReviewModal from '../EditReview/EditReviewModal';
+import './restaurant.css'
+
 const Restaurant = () => {
   const { restaurantId } = useParams();
   const restaurant = useSelector((state) => state.restaurants[+restaurantId]);
   const dispatch = useDispatch();
+
   let reviews;
   const rawReviews = useSelector(
     (state) => state.restaurants[restaurantId]?.reviews
@@ -23,6 +26,7 @@ const Restaurant = () => {
 
   return (
     <div className="restaurant-container">
+      <img src={restaurant.cover_photo} className='coverPhoto'/>
       <h1>{restaurant?.name}</h1>
       <div>
         {reviews?.map((review) => {
