31de5852012b4c536d0c1ada2bead250b51a46af
jas0123uah <jas0123@uah.edu>
2021-12-10 15:46:08 -0500

    Form returns an edit a reservation button.

--- react-app/src/components/CustomerEditReservation/index.js
+++ react-app/src/components/CustomerEditReservation/index.js
@@ -1,22 +1,21 @@
 import React, { useState } from 'react';
 import { Modal } from '../../context/Modal';
-import CustomerReservationForm  from './CustomerEditForm';
+import CustomerEditReservationForm  from './CustomerEditReservationForm';
 
-function CustomerEditReservationModal({arrayOfAvailableDates, availableReservationsArray}) {
+function CustomerEditReservationModal({reservationToEditOrDelete, reservationPartySize, reservationNotes, reservationAvailableSize, reservationRestaurantId}) {
   const [showModal, setShowModal] = useState(false);
 
   const editAReservationButton = (<>
-      <button className="customerEditReservationButton" onClick={() => setShowModal(true)}>Book a reservation</button>
+      <button className="customerEditReservationButton" onClick={() => setShowModal(true)}  data-reservationid={reservationToEditOrDelete} data-partysize={reservationPartySize} data-notes={reservationNotes}>Edit Reservation</button>
       {showModal && (
         <Modal onClose={() => setShowModal(false)}>
-          <CustomerReservationForm arrayOfAvailableDates= {arrayOfAvailableDates} availableReservationsArray={availableReservationsArray} />
+          <CustomerEditReservationForm reservationToEditOrDelete= {reservationToEditOrDelete} reservationPartySize={reservationPartySize} reservationNotes={reservationNotes} reservationAvailableSize={reservationAvailableSize} reservationRestaurantId={reservationRestaurantId} />
         </Modal>
       )}
     </>
   );
 
-  return (arrayOfAvailableDates.length ? editAReservationButton : <h2> No reservations available</h2>
-  );
+  return (editAReservationButton);
 }
 
 export default CustomerEditReservationModal;
