e3b9a3aba3a27ba9363f1fe4c00aade1dbc0cdbf
ashleighctucker <ashleighctucker@gmail.com>
2021-12-11 11:23:16 -0800

    search redux

---
+++ react-app/src/store/search_results.js
@@ -0,0 +1,39 @@
+const LOAD = 'search/LOAD';
+
+const LOAD = (list) => ({
+  type: LOAD,
+  list,
+});
+
+export const getSearch = (term) => async (dispatch) => {
+  const response = await fetch(`/api/search/${term}/`, {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+  if (response.ok) {
+    const results = await response.json();
+    dispatch(load(results['search']));
+    return null;
+  } else if (response.status < 500) {
+    const data = await response.json();
+    if (data.errors) {
+      return data.errors;
+    }
+  } else {
+    return ['An error occurred.'];
+  }
+};
+
+const initialState = [];
+
+export default function SearchReducer(state = initialState, action) {
+  switch (action.type) {
+    case LOAD: {
+      return [...state, ...action.list];
+    }
+    default: {
+      return state;
+    }
+  }
+}
