1974c7dbbd3b2d2d75844389cff9eee864c22c0c
jas0123uah <jas0123@uah.edu>
2021-12-10 20:07:09 -0500

    Seed random reviews.

---
+++ app/seeds/reviews.py
@@ -0,0 +1,23 @@
+from app.models import db, User, Restaurant, Review
+from sqlalchemy.sql.expression import func
+from random import randrange
+from faker import Faker
+#import datetime
+fake = Faker()
+def seed_reviews():
+    
+    for review in range(0,15):
+        user_id = db.session.query(
+            User.id).order_by(func.random()).first()[0]
+        restaurant_obj = db.session.query(Restaurant).filter(Restaurant.user_id != user_id ).order_by(func.random()).first()
+        print(restaurant_obj.id)
+        restaurant_id =restaurant_obj.id
+        rating = randrange(1, 5)
+        comment =  fake.paragraph(nb_sentences=3)
+        seed_review = Review(rating=rating, comment=comment, userId=user_id, restaurantId=restaurant_id)
+        db.session.add(seed_review)
+    db.session.commit()
+    
+def undo_reviews():
+    db.session.execute('TRUNCATE reviews RESTART IDENTITY CASCADE;')
+    db.session.commit()
