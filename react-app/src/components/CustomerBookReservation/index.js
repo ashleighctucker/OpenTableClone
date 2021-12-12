f007edf717550f654ff4e7d28d7c784c7ad9dd4a
ashleighctucker <ashleighctucker@gmail.com>
2021-12-12 14:52:12 -0800

    button changes

--- react-app/src/components/CustomerBookReservation/index.js
+++ react-app/src/components/CustomerBookReservation/index.js
@@ -1,21 +1,41 @@
 import React, { useState } from 'react';
 import { Modal } from '../../context/Modal';
-import CustomerReservationForm  from './CustomerReservationForm';
+import CustomerReservationForm from './CustomerReservationForm';
+import './reservations.css';
 
-function CustomerBookReservationModal({arrayOfAvailableDates, availableReservationsArray}) {
+function CustomerBookReservationModal({
+  arrayOfAvailableDates,
+  availableReservationsArray,
+}) {
   const [showModal, setShowModal] = useState(false);
 
-  const bookAReservationButton = (<>
-      <button className="customerBookReservationButton" onClick={() => setShowModal(true)}>Book a reservation</button>
+  const bookAReservationButton = (
+    <>
+      <button
+        className="customerBookReservationButton"
+        onClick={() => setShowModal(true)}
+      >
+        Book a Reservation
+      </button>
       {showModal && (
         <Modal onClose={() => setShowModal(false)}>
-          <CustomerReservationForm arrayOfAvailableDates= {arrayOfAvailableDates} availableReservationsArray={availableReservationsArray} />
+          <CustomerReservationForm
+            arrayOfAvailableDates={arrayOfAvailableDates}
+            availableReservationsArray={availableReservationsArray}
+          />
         </Modal>
       )}
     </>
   );
 
-  return (arrayOfAvailableDates.length ? bookAReservationButton : <h2> No reservations available</h2>
+  return (
+    <div className="button-div">
+      {arrayOfAvailableDates.length ? (
+        bookAReservationButton
+      ) : (
+        <h2> No reservations available</h2>
+      )}
+    </div>
   );
 }
 
