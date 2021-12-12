cbaac0bab8ec161ea0d1f90ec46fa7beff5c5505
ashleighctucker <ashleighctucker@gmail.com>
2021-12-11 18:08:10 -0800

    removed fave seeds

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -1,9 +1,8 @@
 from flask.cli import AppGroup
-from .users import seed_users, undo_users 
+from .users import seed_users, undo_users
 from .restaurants import seed_restaurants, undo_restaurants
 from .cuisines import seed_cuisines, undo_cuisines
 from .reviews import seed_reviews, undo_reviews
-from .favorites import seed_favorites, undo_favorites
 from .reservations import seed_reservations, undo_reservations
 # Creates a seed group to hold our commands
 # So we can type `flask seed --help`
@@ -17,7 +16,6 @@ def seed():
     seed_cuisines()
     seed_restaurants()
     seed_reviews()
-    seed_favorites()
     seed_reservations()
     # Add other seed functions here
 
@@ -29,6 +27,5 @@ def undo():
     undo_restaurants()
     undo_cuisines()
     undo_reviews()
-    undo_favorites()
     undo_reservations()
     # Add other undo functions here
