78f08e926a8c539eca3bcb6cae9321f60ba397fd
ashleighctucker <ashleighctucker@gmail.com>
2022-01-19 11:12:58 -0800

    changed the validations on login

--- app/forms/login_form.py
+++ app/forms/login_form.py
@@ -4,26 +4,18 @@ from wtforms.validators import DataRequired, Email, ValidationError
 from app.models import User
 
 
-def user_exists(form, field):
-    # Checking if user exists
-    email = field.data
-    user = User.query.filter(User.email == email).first()
-    if not user:
-        raise ValidationError('Login Failed')
-
-
 def password_matches(form, field):
     # Checking if password matches
     password = field.data
     email = form.data['email']
     user = User.query.filter(User.email == email).first()
     if not user:
-        raise ValidationError('Login Failed')
+        raise ValidationError('Invalid username or password')
     if not user.check_password(password):
-        raise ValidationError('Password was incorrect.')
+        raise ValidationError('Invalid username or password')
 
 
 class LoginForm(FlaskForm):
-    email = StringField('email', validators=[DataRequired(), user_exists])
+    email = StringField('email', validators=[DataRequired()])
     password = StringField('password', validators=[
                            DataRequired(), password_matches])
