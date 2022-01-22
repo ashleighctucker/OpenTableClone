3e1fd95f1083f0bdee21b422fe97bf01ecada3a6
ashleighctucker <ashleighctucker@gmail.com>
2022-01-21 19:22:16 -0800

    profile page changes

--- react-app/src/components/CustomerEditReservation/index.js
+++ react-app/src/components/CustomerEditReservation/index.js
@@ -1,9 +1,12 @@
 import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
 import { Modal } from '../../context/Modal';
 import CustomerEditReservationForm from './CustomerEditReservationForm';
 
 function CustomerEditReservationCards({ reservation }) {
+  let restaurants = useSelector((state) => state.restaurants);
   const [showModal, setShowModal] = useState(false);
+  console.log(restaurants, reservation);
 
   let resDate = new Date(reservation.date);
   let realresDate = resDate.setDate(resDate.getDate() + 1);
@@ -17,12 +20,25 @@ function CustomerEditReservationCards({ reservation }) {
 
   return (
     <div className="reservation-div">
-      <span>
-        {reservation.restaurant_name} ({reservation.party_size})
-      </span>
-      <span>Time: {reservation.time_slot}</span>
-      <span>Date: {dateString}</span>
-      <span onClick={() => setShowModal(true)}>
+      <div
+        style={{
+          backgroundImage: `url(${
+            restaurants[reservation.restaurant_id]['cover_photo']
+          })`,
+        }}
+        className="res-photo"
+      ></div>
+      <div className="res-info">
+        <h4>{reservation.restaurant_name}</h4>
+        <span>
+          {dateString} at {reservation.time_slot}
+        </span>
+        <span>
+          Table for {reservation.party_size}{' '}
+          {reservation.party_size === 1 ? 'person' : 'people'}.
+        </span>
+      </div>
+      <span className="edit point" onClick={() => setShowModal(true)}>
         <i className="far fa-edit"></i> Edit Booking
       </span>
       {showModal && (
