ef9d35d13a7ec81ff091d0bcd11d970bdae7fa0c
jas0123uah <jas0123@uah.edu>
2022-02-28 10:15:41 -0500

    Use context to close menu in multiple components

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { ModalProvider } from './context/Modal';
+import {MenuProvider} from './context/Menu'
 import App from './App';
 import configureStore from './store';
 import './index.css';
@@ -12,7 +13,9 @@ ReactDOM.render(
   <React.StrictMode>
     <ModalProvider>
       <Provider store={store}>
+        <MenuProvider>
         <App />
+        </MenuProvider>
       </Provider>
     </ModalProvider>
   </React.StrictMode>,
